6c54ad9e871347d46dad31a3abdb6c5aba0969f6
Ahmed Yaqoob Dhedhi <dhedhiahmedyaqoob@gmail.com>
2024-05-07 14:59:25 +0500

    first commit

---
+++ Word-Counter/index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+console.log(`Your sentence word count is ${words.length}`)
